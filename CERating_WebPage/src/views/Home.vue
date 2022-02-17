2013177442781e8382daa61b1f6dcc5625b7e9b5
starprayer12 <97793476+starprayer12@users.noreply.github.com>
2022-02-17 20:40:51 +0800

    增加跳转

--- CERating_WebPage/src/views/Home.vue
+++ CERating_WebPage/src/views/Home.vue
@@ -22,14 +22,14 @@
             ></div>
           </div>
           <div class="menu-item" :key="1">
-            <a href="/" @click="activeURL = 0">首页</a>
+            <a href="/Home" @click="activeURL = 0">首页</a>
             <div
                 class="menu-underline"
                 :class="{ activedPage: activeURL == 0 }"
             ></div>
           </div>
           <div class="menu-item" :key="2">
-            <a href="/" @click="activeURL = 0">公告</a>
+            <a href="/About" @click="activeURL = 0">公告</a>
             <div
                 class="menu-underline"
                 :class="{ activedPage: activeURL == 0 }"
@@ -52,8 +52,9 @@
         </div>
         <!-- 个人 -->
         <div class="person">
+          <a>登录</a>
           <div class="person-img">
-            <img src="../assets/person.png" alt="" />
+            <a href="/Login" @click="activeURL = 0"><img src="../assets/person.png" alt="" /></a>
           </div>
           <!-- TODO 点击头像下拉框 -->
         </div>
@@ -164,20 +165,20 @@ export default {
   background-size: cover;
   z-index: -1;
 }
-.el-header {
-  width: 100%;
-  height: 50px;
-  padding: 0 20px 0 20px;
-  display: flex;
-  justify-content: space-between;
-  text-align: center;
-  opacity: 0;
-  transition: all 0.5s ease;
-}
-.el-header:hover {
-  padding-top: 15px;
-  opacity: 1;
-}
+/*.el-header {*/
+/*  width: 100%;*/
+/*  height: 50px;*/
+/*  padding: 0 20px 0 20px;*/
+/*  display: flex;*/
+/*  justify-content: space-between;*/
+/*  text-align: center;*/
+/*  opacity: 0;*/
+/*  transition: all 0.5s ease;*/
+/*}*/
+/*.el-header:hover {*/
+/*  padding-top: 15px;*/
+/*  opacity: 1;*/
+/*}*/
 .el-header .logo {
   height: 35px;
 }
