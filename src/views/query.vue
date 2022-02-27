0cff77be840a103de76c558d89e4df2168353445
Aihyn <1301676282@qq.com>
2022-02-27 22:51:43 +0800

    Analog rating navigation

---
+++ src/views/query.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        查询
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Query",
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    watch: {
+
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
