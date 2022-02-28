72ada59ced204a94a1f9f6d0767dbd6a0bffee99
LightInk2020 <crescent2020@163.com>
2022-02-28 16:43:29 +0800

    add message-box and navigation-bar

--- CERating_WebPage/src/views/query.vue
+++ CERating_WebPage/src/views/query.vue
@@ -1,32 +1,29 @@
 <template>
-    <div>
-        查询
-    </div>
+  <div>
+    <NavigationBar />
+    查询
+    <MessageBox :fTitle="'温馨提示'" :fMessage="'是否已经了解上述内容？如有疑问请先查看模拟评级导航'" :fGoName="'进入模拟评级'" :fGoURL="'/'"/>
+  </div>
 </template>
 
 <script>
+import NavigationBar from "../components/NavigationBar.vue";
+import MessageBox from "../components/MessageBox.vue"
+
+
 export default {
-    name: "Query",
-    props: {
-
-    },
-    data() {
-        return {
-
-        };
-    },
-    watch: {
-
-    },
-    methods: {
-
-    },
-    components: {
-
-    },
+  name: "Query",
+  props: {},
+  data() {
+    return {};
+  },
+  watch: {},
+  methods: {},
+  components: {
+    NavigationBar,
+    MessageBox
+  },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
