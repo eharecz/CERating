7012819c55957957693b965cdbca639390653ff7
FORIMOC <73113688+FORIMOC@users.noreply.github.com>
2022-03-03 12:38:04 +0800

    consult_pages

---
+++ CERating_WebPage/src/components/Consult/MainTitle.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "MainTitle"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ CERating_WebPage/src/views/ConsultDetail.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "ConsultDetail"
+}
+</script>
+
+<style scoped>
+
+</style>
