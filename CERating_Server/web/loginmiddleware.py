4f36d91d3cc141e69cd73a932a12a9e825b7b32d
what-yes <56341258+what-yes@users.noreply.github.com>
2022-03-04 18:58:36 +0800

    充值接口

--- CERating_Server/web/loginmiddleware.py
+++ CERating_Server/web/loginmiddleware.py
@@ -12,16 +12,10 @@ class LoginMiddleware:
         # path = request.path
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-<<<<<<< HEAD
         urllist = ['/enterprise_login/']
         # if not re.match(r'^/enterprise_login/',path):
             # return redirect(reverse("enterprise_login/"))
 
-=======
-        # urllist = ['/enterprise_login/']
-        # if not request.session.get('is_login',None) or not re.match(r'^/enterprise_login/',path):
-        #     return redirect(reverse("enterprise_login/"))
->>>>>>> rz
         response = self.get_response(request)
 
         # Code to be executed for each request/response after
